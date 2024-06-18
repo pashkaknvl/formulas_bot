b4a443098cdc65a956364f5f6b63f07ae28bd67c
Pasha Konovalov <pashak.konovalov05@gmail.com>
2024-06-18 20:54:20 +0300

    added keyboards

--- include/constants.h
+++ include/constants.h
@@ -6,6 +6,8 @@
 #define INCLUDE_CONSTANTS_H_
 
 #include <string>
+#include <tgbot/tgbot.h>
+#include <vector>
 
 namespace consts {
 // TODO(diduk001): проверять регексом в compile-time
@@ -15,6 +17,32 @@ constexpr char DB_NAME[] = "../chinook.db";
 
 namespace messages {
 constexpr char HI[] = "Hi!";
+}
+
+namespace keyboards {
+// менюшка, появляющаяся при нажатии на кнпоку в меню выбора группы (неизменяемая)
+TgBot::InlineKeyboardButton::Ptr subjects_btn(new TgBot::InlineKeyboardButton{"Посмотреть предметы", {}, "view_subjects"}); 
+TgBot::InlineKeyboardButton::Ptr change_group_btn(new TgBot::InlineKeyboardButton{"Изменить параметры группы", {}, "change_group"});
+std::vector<TgBot::InlineKeyboardButton::Ptr> buttons_GroupActions = {subjects_btn, change_group_btn, delete_group_btn};
+TgBot::InlineKeyboardMarkup::Ptr keyboard_GroupActions(new TgBot::InlineKeyboardMarkup{buttons_GroupActions});
+
+// менюшка, появляющаяся при нажатии на кнопку просмотра предметов(изменяемая(кол-во предметов))
+TgBot::InlineKeyboardButton::Ptr GoToGroupActions_btn(new TgBot::InlineKeyboardButton{"Назад", {}, "go_to_group_actions"}); 
+TgBot::InlineKeyboardButton::Ptr AddSubject_btn(new TgBot::InlineKeyboardButton{"Добавить предмет", {}, "add_subject"}); 
+// короче тут надо ещё добавить n-ое количесвто кнопок с названиями предметов, подсосав их из бд и не забыть закинуть их в вектор
+std::vector<TgBot::InlineKeyboardButton::Ptr> buttons_ListOfSubjects = {AddSubject_btn, GoToGroupActions_btn};
+TgBot::InlineKeyboardMarkup::Ptr keyboard_ViewSubjects(new TgBot::InlineKeyboardMarkup{buttons_ListOfSubjects}); 
+
+// менюшка, появляющаяся при нажатии на кнопку предмета(неизменяемая)
+TgBot::InlineKeyboardButton::Ptr Info_btn(new TgBot::InlineKeyboardButton{"Информация о предмете", {}, "show_info"}); 
+TgBot::InlineKeyboardButton::Ptr Evaluate_btn(new TgBot::InlineKeyboardButton{"Расчитать по формуле", {}, "evaluate"}); 
+TgBot::InlineKeyboardButton::Ptr Elements_btn(new TgBot::InlineKeyboardButton{"Элементы контроля", {}, "elements_subject"}); 
+TgBot::InlineKeyboardButton::Ptr ChangeSubject_btn(new TgBot::InlineKeyboardButton{"Изменить параметры", {}, "change_subject"}); 
+TgBot::InlineKeyboardButton::Ptr DeleteSubject_btn(new TgBot::InlineKeyboardButton{"Удалить предмет", {}, "delete_subject"}); 
+std::vector<TgBot::InlineKeyboardButton::Ptr> buttons_SubjectActions = {Info_btn, Evaluate_btn, ChangeSubject_btn, DeleteSubject_btn};
+TgBot::InlineKeyboardMarkup::Ptr keyboard_SubjectActions(new TgBot::InlineKeyboardMarkup{buttons_SubjectActions});
+
+
 }
 
 namespace db_queries {
